8e4392466e544444148390fed8bccc4b856e8dd7
surejasmit <smitsureja472007@gmail.com>
2026-04-13 09:11:50 +0530

    change main

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 from fastapi import FastAPI
 import joblib
 import os
+
 app = FastAPI()
 
+# Load model
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(BASE_DIR, "model.pkl")
 
 model = joblib.load(model_path)
 
+
 @app.get("/")
 def read_root():
-    return {"message": "ML API running !"}
+    return {"message": "ML API running!"}
+
 
 @app.post("/predict")
 def predict(data: dict):
-    features = [[data["feature1"], data["feature2"]]]
-    prediction = model.predict(features)
-    return {"prediction": prediction[0]}
+    try:
+        feature1 = data["feature1"]
+        feature2 = data["feature2"]
+
+        features = [[feature1, feature2]]
+
+        prediction = model.predict(features)
+
+        return {
+            "prediction": float(prediction[0])
+        }
+
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
