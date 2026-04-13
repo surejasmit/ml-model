b3201d210351907a920644856bc3a8131d34c77d
surejasmit <smitsureja472007@gmail.com>
2026-04-13 09:08:38 +0530

    chaneg code

--- train.py
+++ train.py
@@ -2,10 +2,21 @@
 from sklearn.linear_model import LinearRegression
 import joblib
 
-X = [[1], [2], [3], [4]]
+# Training data (2 features)
+X = [
+    [1, 10],
+    [2, 20],
+    [3, 30],
+    [4, 40]
+]
+
 y = [100, 200, 300, 400]
 
+# Train model
 model = LinearRegression()
 model.fit(X, y)
 
+# Save model
 joblib.dump(model, "model.pkl")
+
+print("✅ Model trained and saved as model.pkl")
